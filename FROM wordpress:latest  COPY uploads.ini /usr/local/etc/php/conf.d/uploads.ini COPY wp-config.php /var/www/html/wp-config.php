b368f90e47a1f3ddee4076d232d7de0618d651e9
fuongnguyen60-source <fuongnguyen60@gmail.com>
2025-08-26 10:28:24 +0700

    Create wp-config.php

---
+++ FROM wordpress:latest  COPY uploads.ini /usr/local/etc/php/conf.d/uploads.ini COPY wp-config.php /var/www/html/wp-config.php
@@ -0,0 +1,23 @@
+<?php
+define('DB_NAME', 'railway');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'egaKugbvlhjyXmjdFgfLCgjLXjbEvoAf');
+define('DB_HOST', 'gondola.proxy.rlwy.net:31065');
+
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+$table_prefix = 'wp_';
+define('WP_DEBUG', false);
+
+define('WP_HOME', 'https://bh-demo-production-a904.up.railway.app');
+define('WP_SITEURL', 'https://bh-demo-production-a904.up.railway.app');
+
+// Nếu server chạy sau proxy (Railway), bắt buộc bật HTTPS thủ công
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+if (!defined('ABSPATH')) {
+  define('ABSPATH', __DIR__ . '/');
+}
+require_once ABSPATH . 'wp-settings.php';
