fbbe19b3ea5c88c001a9f0e9125ad641fd221399
fuongnguyen60-source <fuongnguyen60@gmail.com>
2025-08-26 10:39:16 +0700

    Update wp-config.php

--- FROM wordpress:latest  COPY uploads.ini /usr/local/etc/php/conf.d/uploads.ini COPY wp-config.php /var/www/html/wp-config.php
+++ FROM wordpress:latest  COPY uploads.ini /usr/local/etc/php/conf.d/uploads.ini COPY wp-config.php /var/www/html/wp-config.php
@@ -9,9 +9,6 @@ define('DB_COLLATE', '');
 $table_prefix = 'wp_';
 define('WP_DEBUG', false);
 
-define('WP_HOME', 'https://bh-demo-production-a904.up.railway.app');
-define('WP_SITEURL', 'https://bh-demo-production-a904.up.railway.app');
-
 // Nếu server chạy sau proxy (Railway), bắt buộc bật HTTPS thủ công
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
